c0e94e354de4e8690bdaf1c41d0eb63ad4e3a8f5
Sojtin <sojtin@gmail.com>
2021-06-20 21:48:24 +0200

    panic err recover

--- main.go
+++ main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	helper "drvcheck/drvcheck"
+	"fmt"
 	// "fmt"
 	// "github.com/goccy/go-yaml"
 )
 
 func main() {
 
+	defer func() {
+		panic_err := recover()
+		if panic_err != nil {
+			fmt.Println("Panic", panic_err)
+		}
+	}()
 	// todo: executable?
 	// todo: http://golang.org.pl/lang/08_oop.html
 	helper.Run()
